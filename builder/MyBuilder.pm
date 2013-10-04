61371c706b8aeea1f412077545907e25b4205e8e
tokuhirom <tokuhirom@gmail.com>
2013-10-04 13:59:05 +0900

    Check availability about accept4() system call.

--- builder/MyBuilder.pm
+++ builder/MyBuilder.pm
@@ -4,11 +4,22 @@ use warnings;
 use utf8;
 use 5.010_001;
 use parent qw(Module::Build);
+use Devel::CheckCompiler 0.03;
 
 sub new {
     my $self = shift;
     if ($^O ne 'linux') {
         print "This module only supports linux.\n";
+        exit 0;
+    }
+    if (check_compile(<<'...') != 1) {
+#include <sys/syscall.h>
+
+#if !defined __NR_accept4
+#error "Your environment does not supports accept4."
+#endif
+...
+        print "This module only supports linux 2.6.28+ and glibc 2.10+.\n";
         exit 0;
     }
     $self->SUPER::new(@_);
--- cpanfile
+++ cpanfile
@@ -2,6 +2,10 @@ requires 'perl', '5.010001';
 requires 'Exporter';
 requires 'parent';
 
+on 'configure' => sub {
+    requires 'Devel::CheckCompiler';
+};
+
 on 'test' => sub {
     requires 'Test::More', '0.98';
     requires 'Test::TCP', '2.01';
